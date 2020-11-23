674f354cc698a1916ed5efb2b1e9699e6dd499bf
caroluces <carolina_luces@hotmail.com>
2020-11-22 20:32:08 -0800

    header changed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-function Card3(){
+function Card1(){
     alert("Hi carolina, kevin loves you   <3")
+}
+function Card2(){
+    alert("Yo tb te quiero")
+}
+function Card3(){
+    alert("CaroLuces")
 }
