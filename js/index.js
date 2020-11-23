dd83925b67201f280413ffd9005e7614b9d6c04c
Kevin Nava <hombrehealth@hotmail.com>
2020-11-23 00:06:41 -0400

    frontend bug fixed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 function Card3(){
     alert("Hi carolina, kevin loves you   <3")
 }
+
+
+function Card2(){
+    alert("You are amazing!!!")
+}
+
+
+function Card1(){
+    alert("You are the best!!!")
+}
